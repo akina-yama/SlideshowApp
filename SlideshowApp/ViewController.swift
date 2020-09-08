230e22b386629b64ef0695a151c60c5a18c8a356
aki.yamanishi <snowdrop01s04a@yahoo.co.jp>
2020-09-09 02:25:03 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Akina Yamanishi on 2020/09/09.
+//  Copyright © 2020 Akina.Yamanishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
